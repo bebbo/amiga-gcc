8f5cf79c397b14770382789791bcef12b278a355
bebbo <bebbo@bejy.net>
2021-10-05 13:10:22 +0200

    poke min version of MUI to 19

---
+++ patches/mui/SDK/MUI/C/include/libraries/mui.h
@@ -0,0 +1,11 @@
+--- SDK/MUI/C/include/libraries/mui.h	2020-12-28 10:47:33.000000000 +0100
++++ SDK/MUI/C/include/libraries/mui.h	2021-10-05 11:25:11.067182854 +0200
+@@ -153,7 +153,7 @@
+ ***************************************************************************/
+ 
+ #define MUIMASTER_NAME    "muimaster.library"
+-#define MUIMASTER_VMIN    20
++#define MUIMASTER_VMIN    19
+ #define MUIMASTER_VLATEST 20
+ 
+ /*
